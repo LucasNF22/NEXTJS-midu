2f9346dc4e645ce31ec5bffbebe8e43ee3ad22f6
LucasNF22 <lucasnfiorentino@gmail.com>
2024-01-26 20:54:33 -0300

    Componente Link, hooks de nextJS, 'use client' 'use server', base de datos en vercel SQL, fetching de datos a la base.

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
-export default function DashboardPage() {
+import { fetchRevenue } from "../lib/data"
+
+export default async function DashboardPage() {
+
+    const reveneu = await fetchRevenue();
+
+    console.log(reveneu);
+    
+
     return <p>Esta es la pagina del dashbaord</p>
 }
